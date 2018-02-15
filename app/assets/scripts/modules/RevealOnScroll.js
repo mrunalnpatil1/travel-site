4c6551cc8ce7294ad0290e7812aacc52400e0c36
Mrunal <mrunalnpatil1@gmail.com>
2018-02-16 00:23:32 +0100

    Completed, reval-on-scroll functionality

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    this.OffsetPercentage = offset;
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
+  }
+
+  createWaypoints() {
+    var that = this;/*inside the new waypoint "this" refers to new object, so store current object in variable */
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass("reveal-item--is-visible");
+        },
+        offset: that.OffsetPercentage/*after 15% of div is exposed on screen, then handler adds the class to reveal the element*/
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
